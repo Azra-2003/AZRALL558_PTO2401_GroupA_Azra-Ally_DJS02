cffed0708fa6f131bd79853ce3ea9f1511b51778
Azra Ally <allyazra2003@gmail.com>
2024-08-30 17:43:11 +0200

    complete

--- scripts.js
+++ scripts.js
@@ -1,13 +1,3 @@
-//const form = document.querySelector("[data-form]");
-//const result = document.querySelector("[data-result]");
-
-//form.addEventListener("submit", (event) => {
-  //event.preventDefault();
-  //const entries = new FormData(event.target);
-  //const { dividend, divider } = Object.fromEntries(entries);
-  //result.innerText = dividend / divider;
-//});
-
 const form = document.querySelector("[data-form]");
 const result = document.querySelector("[data-result]");
 
